da2580cc989465f710c1da2b7f2d4612765ee51e
yuhwanwoo <6514687@naver.com>
2024-03-21 21:28:57 +0900

    ch1 init

---
+++ src/main/java/com/task/object/ch1/Invitation.java
@@ -0,0 +1,7 @@
+package com.task.object.ch1;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/main/java/com/task/object/ch1/Ticket.java
@@ -0,0 +1,9 @@
+package com.task.object.ch1;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
