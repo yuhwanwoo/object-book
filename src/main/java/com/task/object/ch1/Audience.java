b26bd187636be341ebf8d7f1ab9082c1cad10b35
hwanwoo <hwanwoo@rowan.kr>
2024-03-22 19:20:58 +0900

    예상이 빗나가고 변경에 취약한 코드

---
+++ src/main/java/com/task/object/ch1/Audience.java
@@ -0,0 +1,13 @@
+package com.task.object.ch1;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
