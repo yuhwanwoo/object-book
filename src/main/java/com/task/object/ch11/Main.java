ad9f557cdb571bf4f60032fb3bbe41b45ae8a5db
yuhwanwoo <6514687@naver.com>
2024-06-12 07:05:01 +0900

    합성을 이용한 처리

---
+++ src/main/java/com/task/object/ch11/Main.java
@@ -0,0 +1,10 @@
+package com.task.object.ch11;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        상속은 결합도를 증가시키지만
+        합성은 구현에 의존하지 않아 그렇지 않다.
+         */
+    }
+}
